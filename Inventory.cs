b34f085d3a5d33feec175cae826cb2c3df055ee2
Ibrahim Sadi <ibrahimsadi103@gmail.com>
2025-03-19 23:16:09 +0200

    Refactor the code and validate user input

--- Inventory.cs
+++ Inventory.cs
@@ -1,98 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FTS.SimpleInventoryManagementSystem;
 
-namespace FTS.SimpleInventoryManagementSystem
+public class Inventory
 {
-    class Inventory
-    {
-        private List<Product> products = new List<Product>();
+    public List<Product> Products { get; } = [];
 
-        public void AddProduct(Product product)
+    public void AddProduct(Product product)
+    {
+        try
         {
-            products.Add(product);
+            Products.Add(product);
             Console.WriteLine("Product added successfully!");
         }
-
-        public void ViewProducts()
-        {
-            Console.WriteLine();
-
-            if (products.Count == 0)
-            {
-                Console.WriteLine("No products in inventory.");
-                return;
-            }
-
-            Console.WriteLine("Inventory List:");
-            foreach (var product in products)
-            {
-                Console.WriteLine(product);
-            }
-        }
-
-        public void EditProduct(string name)
-        {
-            Product? product = products.Find(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-
-            if (product == null)
-            {
-                Console.WriteLine("Product not found.");
-                return;
-            }
-
-            Console.Write($"Current product name: {product.Name}. Enter new name (or press enter to keep the same): ");
-            string newName = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(newName))
-            {
-                product.Name = newName;
-            }
-
-            Console.Write($"Current product quantity: {product.AmountInStock}. Enter new quantity (or press enter to keep the same): ");
-            string newQuantityInput = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(newQuantityInput) && int.TryParse(newQuantityInput, out int newQuantity))
-            {
-                product.AmountInStock = newQuantity;
-            }
-
-            Console.Write($"Current prdouct price: {product.Price}. Enter new price (or press enter to keep the same): ");
-            string newPriceInput = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(newPriceInput) && decimal.TryParse(newPriceInput, out decimal newPrice))
-            {
-                product.Price = newPrice;
-            }
-
-            Console.WriteLine("Product updated successfully!");
-        }
         
-        public void DeleteProduct(string name)
+        catch (Exception ex)
         {
-            int index = products.FindIndex(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-            
-            if (index != -1)
-            {
-                products.RemoveAt(index);
-                Console.WriteLine($"Successfully deleted the {name.ToLower()} product");
-            } else
-            {
-                Console.WriteLine("Product was not found");
-            }
+            Console.WriteLine($"Error adding product: {ex.Message}");
+        }
+    }
+
+    public void ViewProducts()
+    {
+        Console.WriteLine();
+
+        if (Products.Count == 0)
+        {
+            Console.WriteLine("No products in inventory.");
+            return;
         }
 
-        public void SearchProduct(string name)
+        Console.WriteLine("Inventory List:");
+        foreach (var product in Products)
         {
-            Product? product = products.Find(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-
-            if (product == null)
-            {
-                Console.WriteLine("Product not found.");
-                return;
-            }
-
-            Console.WriteLine("\nSearch Result:");
             Console.WriteLine(product);
         }
+    }
+
+    public void EditProduct(string name)
+    {
+        var product = Products.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        
+        if (product is null)
+        {
+            Console.WriteLine("Product not found.");
+            return;
+        }
+
+        Console.Write($"Current product name: {product.Name}. Enter new name (or press enter to keep the same): ");
+        var newName = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(newName))
+        {
+            product.Name = newName;
+        }
+
+        Console.Write($"Current product quantity: {product.AmountInStock}. Enter new quantity (or press enter to keep the same): ");
+        var newQuantityInput = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(newQuantityInput) && int.TryParse(newQuantityInput, out int newQuantity))
+        {
+            product.AmountInStock = newQuantity;
+        }
+
+        Console.Write($"Current prdouct price: {product.Price}. Enter new price (or press enter to keep the same): ");
+        var newPriceInput = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(newPriceInput) && decimal.TryParse(newPriceInput, out decimal newPrice))
+        {
+            product.Price = newPrice;
+        }
+
+        Console.WriteLine("Product updated successfully!");
+    }
+        
+    public void DeleteProduct(string name)
+    {
+        var index = Products.FindIndex(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            
+        if (index is not -1)
+        {
+            Products.RemoveAt(index);
+            Console.WriteLine($"Successfully deleted the {name.ToLower()} product");
+        }
+        else
+        {
+            Console.WriteLine("Product was not found");
+        }
+    }
+
+    public void SearchProduct(string name)
+    {
+        var product = Products.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        if (product is null)
+        {
+            Console.WriteLine("Product not found.");
+            return;
+        }
+
+        Console.WriteLine("\nSearch Result:");
+        Console.WriteLine(product);
     }
 }
--- Product.cs
+++ Product.cs
@@ -1,37 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FTS.SimpleInventoryManagementSystem;
 
-namespace FTS.SimpleInventoryManagementSystem
+public class Product
 {
-    public class Product
+    private string name = string.Empty;
+
+    public int AmountInStock { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Name
     {
-        private string name = String.Empty;
-        public int AmountInStock { get; set; }
-
-        public decimal Price { get; set; }
-
-        public string Name
+        get { return name; }
+        set
         {
-            get { return name; }
-            set
-            {
-                name = value.Length > 50 ? value[..50] : value;
-            }
+            name = value.Length > 50 ? value[..50] : value;
+        }
+    }
+
+    public Product(string name, int quantity, decimal productPrice)
+    {
+        if (!IsValidObject(name, quantity, productPrice))
+        {
+            throw new ArgumentException("Invalid product values. Name must be alphabetic, and quantity/price must be non-negative.");
         }
 
-        public Product(string name, int quantity, decimal productPrice)
+        Name = name;
+        AmountInStock = quantity;
+        Price = productPrice;
+    }
+
+    private bool IsValidObject(string name, int quantity, decimal price)
+    {
+        if (string.IsNullOrWhiteSpace(name) || !name.All(char.IsLetter))
         {
-            Name = name;
-            AmountInStock = quantity;
-            Price = productPrice;
+            return false;
+        }
+        
+        if (quantity < 0 || price < 0)
+        {
+            return false;
         }
 
-        public override string ToString()
-        {
-            return $"Name: {Name}, Quantity: {AmountInStock}, Price: {Price}";
-        }
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Quantity: {AmountInStock}, Price: {Price}";
     }
 }
